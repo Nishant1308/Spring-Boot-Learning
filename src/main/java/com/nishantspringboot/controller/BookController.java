75ae03358746c89adf3d90fced0435553644e724
Nishant Gaurav <50671536+Nishant1308@users.noreply.github.com>
2024-07-23 23:30:46 +0530

    create, get, update and delete method code

--- src/main/java/com/nishantspringboot/controller/BookController.java
+++ src/main/java/com/nishantspringboot/controller/BookController.java
@@ -3,8 +3,12 @@ package com.nishantspringboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nishantspringboot.entity.Book;
@@ -15,14 +19,31 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
-	
+
 	@GetMapping("/books")
 	public List<Book> getBooks() {
 		return this.bookService.getAllBooks();
 	}
-	
+
 	@GetMapping("/book/{id}")
 	public Book getBookById(@PathVariable("id") int id) {
 		return this.bookService.getBookById(id);
+	}
+
+	@PostMapping("/addBook")
+	public Book addBook(@RequestBody Book book) {
+		this.bookService.addBook(book);
+		return book;
+	}
+
+	@DeleteMapping("/deleteBook/{id}")
+	public void deleteBook(@PathVariable("id") int id) {
+		this.bookService.deleteBook(id);
+	}
+
+	@PutMapping("/updateBook/{id}")
+	public Book updateBook(@RequestBody Book book, @PathVariable("id") int id) {
+		this.bookService.updateBook(book, id);
+		return book;
 	}
 }
--- src/main/java/com/nishantspringboot/service/BookService.java
+++ src/main/java/com/nishantspringboot/service/BookService.java
@@ -2,6 +2,7 @@ package com.nishantspringboot.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -11,22 +12,43 @@ import com.nishantspringboot.entity.Book;
 public class BookService {
 
 	private static List<Book> list = new ArrayList<>();
-	
+
 	static {
-		list.add(new Book(4567,"ghjkl","rtyui"));
-		list.add(new Book(98765,"rtyuio","ertyu"));
-		list.add(new Book(987,"fghjk","rtyu"));
+		list.add(new Book(4567, "ghjkl", "rtyui"));
+		list.add(new Book(98765, "rtyuio", "ertyu"));
+		list.add(new Book(987, "fghjk", "rtyu"));
 	}
-	
-	//get All books
-	public List<Book> getAllBooks(){
+
+	// get All books
+	public List<Book> getAllBooks() {
 		return list;
 	}
-	
-	//get book by id
+
+	// get book by id
 	public Book getBookById(int id) {
 		Book book = null;
-		book = list.stream().filter(e-> e.getId()==id).findFirst().get();
+		book = list.stream().filter(e -> e.getId() == id).findFirst().get();
 		return book;
+	}
+
+	// add book
+	public void addBook(Book b) {
+		list.add(b);
+	}
+	
+	//delete book
+	public void deleteBook(int id) {
+		list = list.stream().filter(e->e.getId()!=id).collect(Collectors.toList());
+	}
+	
+	//update book
+	public void updateBook(Book book, int id) {
+		list = list.stream().map(b->{
+			if(b.getId()==id) {
+				b.setAuthor(book.getAuthor());
+				b.setTitle(book.getTitle());
+			}
+			return b;
+		}).collect(Collectors.toList()); 
 	}
 }
