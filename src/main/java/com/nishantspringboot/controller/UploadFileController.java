8ab59f7ebf3ba540963996d760ea203c138efbaf
Nishant Gaurav <50671536+Nishant1308@users.noreply.github.com>
2024-07-27 23:36:07 +0530

    dynamic file upload

--- src/main/java/com/nishantspringboot/controller/UploadFileController.java
+++ src/main/java/com/nishantspringboot/controller/UploadFileController.java
@@ -7,29 +7,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nishantspringboot.helper.FileUploadHelper;
 
 @RestController
 public class UploadFileController {
-	
+
 	@Autowired
 	private FileUploadHelper fileUploadHelper;
-	
+
 	@PostMapping("/uploadFile")
-	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
-		
+	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+
 		try {
-			if(file.isEmpty()) {
+			if (file.isEmpty()) {
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Request Must contain file");
 			}
-			
+
 			boolean b = fileUploadHelper.uploadFile(file);
-			
-			if(b) {
-				return ResponseEntity.ok("File is successfully uploaded");
+
+			if (b) {
+				return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/image/")
+						.path(file.getOriginalFilename()).toUriString());
 			}
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong !! try again");
--- src/main/java/com/nishantspringboot/helper/FileUploadHelper.java
+++ src/main/java/com/nishantspringboot/helper/FileUploadHelper.java
@@ -2,33 +2,45 @@ package com.nishantspringboot.helper;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUploadHelper {
-	public final String upload_dir = "C:\\Users\\USER\\eclipse-workspace\\nishantspringboot\\src\\main\\resources\\static\\image\\";
-	
+
+	// static file path
+	// public final String upload_dir =
+	// "C:\\Users\\USER\\eclipse-workspace\\nishantspringboot\\src\\main\\resources\\static\\image\\";
+
+	// dynamic file path
+	public final String upload_dir = new ClassPathResource("static/image/").getFile().getAbsolutePath();
+
+	public FileUploadHelper() throws IOException {
+
+	}
+
 	public boolean uploadFile(MultipartFile file) {
 		boolean flag = false;
 		try {
-			InputStream is= file.getInputStream();
+			InputStream is = file.getInputStream();
 			byte[] data = new byte[is.available()];
 			is.read(data);
-			
-			FileOutputStream fos = new FileOutputStream(upload_dir+File.separator+file.getOriginalFilename());
+
+			FileOutputStream fos = new FileOutputStream(upload_dir + File.separator + file.getOriginalFilename());
 			fos.write(data);
 			fos.flush();
 			fos.close();
-			
+
 			flag = true;
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return flag;
 	}
 
